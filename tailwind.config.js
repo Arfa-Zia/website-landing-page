9e90723d06b07ea29a6c20863e6a8f84f8dc24b7
Arfa Zia <arfazia2006@gmail.com>
2024-05-14 22:50:03 +0500

    Add extend properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-shade': {
+          1: '#43A046' , 
+          2: '#388e3b' ,
+          3: '#237d31' ,
+          4: '#1b5e1f' ,
+          5: '#103e13' ,
+        },
+        neutral: {
+          silver: '#F5F7FA',
+        }
+      },
+      fontFamily: {
+        inter: ['Inter',...defaultTheme.fontFamily.sans],
+
+      }
+    },
   },
   plugins: [],
 }
